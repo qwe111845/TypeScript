0664ff1519bd0070a339c7dfccb05eef69f87fbd
qwe111845 <qwe111845@gmail.com>
2021-11-14 15:43:45 +0800

    feat: first commit
    This project is the basis for practicing typescript

---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,16 @@
+class Customer {
+
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+let myCustomer = new Customer("Martin", "Dixon");
+
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
